fba463174199ff002922ad299031a99e66a819d6
WhaleFall <63758849+WhaleFell@users.noreply.github.com>
2024-12-03 05:47:02 +0000

    custom openwrt compile.

---
+++ env_build.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository ppa:npalix/coccinelle
+sudo apt update
+sudo apt install build-essential clang flex g++ gawk gcc-multilib gettext \
+git libncurses5-dev libssl-dev python3-distutils rsync unzip zlib1g-dev \
+coccinelle
