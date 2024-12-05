361d6a213ca4c9152b31fe9c1862eb5e228fe744
whalefell <63758849+WhaleFell@users.noreply.github.com>
2024-12-06 00:18:31 +0800

    Fix: Update preprocess config shell script to replace dnsmasq

--- preprocess_config.sh
+++ preprocess_config.sh
@@ -16,3 +16,21 @@ cat <<EOF | sed -E 's/^  //' >> .config
   CONFIG_PACKAGE_luci=y
 EOF
 make defconfig
+
+# replace dnsmasq to dnsmasq-full
+# .config file path
+CONFIG_FILE=".config"
+
+# check .config is exist
+if [[ ! -f $CONFIG_FILE ]]; then
+    echo "Error: $CONFIG_FILE not found!"
+    exit 1
+fi
+
+
+sed -i 's/^CONFIG_DEFAULT_dnsmasq=y/# &/' $CONFIG_FILE
+sed -i 's/^CONFIG_MODULE_DEFAULT_dnsmasq=y/# &/' $CONFIG_FILE
+sed -i 's/^CONFIG_PACKAGE_dnsmasq=m/# CONFIG_PACKAGE_dnsmasq is not set/' $CONFIG_FILE
+sed -i 's/^CONFIG_PACKAGE_dnsmasq=y/# CONFIG_PACKAGE_dnsmasq is not set/' $CONFIG_FILE
+
+echo "Replace dnsmasq to dnsmasq-full done."
