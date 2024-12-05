050ac01c3437a9bf9151300734f62d2341bc91f3
whalefell <63758849+WhaleFell@users.noreply.github.com>
2024-12-05 14:12:00 +0800

    Chore:

--- hellowall.sh
+++ hellowall.sh
@@ -1,6 +1,8 @@
-rm -rf package/helloworld
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+# rm -rf package/helloworld
+# git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 
-git -C package/helloworld pull
+# git -C package/helloworld pull
 
-rm -rf package/helloworld/shadowsocks-rust
+# rm -rf package/helloworld/shadowsocks-rust
+
+echo "Hellowall was Deprecated in this commit now."
